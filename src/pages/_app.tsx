1033f0a60300887205eaa9c6c539ff33d245ff4f
chiaraferrara <123738595+choorywey@users.noreply.github.com>
2024-03-03 11:24:55 +0100

    feat: redux toolkit store and slice

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
 import "@/styles/globals.css";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 import type { AppProps } from "next/app";
 
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: 0,
+  reducers: {
+    increment: (state, action) => state + action.payload,
+    decrement: (state, action) => state - action.payload,
+  },
+
+});
+
+  const store = configureStore({
+    reducer: {
+      counter: counterSlice.reducer,
+    },
+  });
+
 export default function App({ Component, pageProps }: AppProps) {
+
   return <Component {...pageProps} />;
 }
